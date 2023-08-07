7cd9cb1b4e672d6332309f1cf31c07753bbcdb20
NSD <73041332+Nisedo@users.noreply.github.com>
2023-08-07 18:07:28 +0200

    Deployment Cost 746991 - Deployment Size 4150

--- GasOptimisationFoundry/src/Gas.sol
+++ GasOptimisationFoundry/src/Gas.sol
@@ -16,33 +16,9 @@ contract GasContract is Ownable {
     mapping(address => uint256) private isOddWhitelistUser;
     mapping(address => ImportantStruct) private whiteListStruct;
 
-    enum PaymentType {
-        Unknown,
-        BasicPayment,
-        Refund,
-        Dividend,
-        GroupPayment
-    }
-
-    struct Payment {
-        PaymentType paymentType;
-        uint256 paymentID;
-        bool adminUpdated;
-        string recipientName; // max 8 characters
-        address recipient;
-        address admin; // administrators address
-        uint256 amount;
-    }
-
-    struct ImportantStruct {
-        uint256 amount;
-        uint256 bigValue;
-        uint8 valueA; // max 3 digits
-        uint8 valueB; // max 3 digits
-        bool paymentStatus;
-        address sender;
-    }
-
+    event supplyChanged(address indexed, uint256 indexed);
+    event Transfer(address recipient, uint256 amount);
+    event WhiteListTransfer(address indexed);
     event AddedToWhitelist(address userAddress, uint256 tier);
 
     modifier onlyAdminOrOwner() {
@@ -65,27 +41,50 @@ contract GasContract is Ownable {
         _;
     }
 
-    event supplyChanged(address indexed, uint256 indexed);
-    event Transfer(address recipient, uint256 amount);
-    event WhiteListTransfer(address indexed);
+    enum PaymentType {
+        Unknown,
+        BasicPayment,
+        Refund,
+        Dividend,
+        GroupPayment
+    }
+
+    struct Payment {
+        PaymentType paymentType;
+        uint8 paymentID;
+        bool adminUpdated;
+        string recipientName; // max 8 characters
+        address recipient;
+        address admin; // administrators address
+        uint256 amount;
+    }
+
+    struct ImportantStruct {
+        uint256 amount;
+        uint256 bigValue;
+        uint8 valueA; // max 3 digits
+        uint8 valueB; // max 3 digits
+        bool paymentStatus;
+        address sender;
+    }
 
     constructor(address[] memory _admins, uint256 _totalSupply) payable {
         unchecked {
             contractOwner = msg.sender;
             totalSupply = _totalSupply;
 
-            for (uint256 ii; ii < administrators.length; ++ii) {
-                if (_admins[ii] != address(0)) {
-                    administrators[ii] = _admins[ii];
-                    if (_admins[ii] == contractOwner) {
+            for (uint256 i; i < administrators.length; ++i) {
+                if (_admins[i] != address(0)) {
+                    administrators[i] = _admins[i];
+                    if (_admins[i] == contractOwner) {
                         balances[contractOwner] = totalSupply;
                     } else {
-                        balances[_admins[ii]];
+                        balances[_admins[i]];
                     }
-                    if (_admins[ii] == contractOwner) {
-                        emit supplyChanged(_admins[ii], totalSupply);
-                    } else if (_admins[ii] != contractOwner) {
-                        emit supplyChanged(_admins[ii], 0);
+                    if (_admins[i] == contractOwner) {
+                        emit supplyChanged(_admins[i], totalSupply);
+                    } else if (_admins[i] != contractOwner) {
+                        emit supplyChanged(_admins[i], 0);
                     }
                 }
             }
@@ -95,8 +94,8 @@ contract GasContract is Ownable {
     function checkForAdmin(address _user) public view returns (bool admin_) {
         unchecked {
             bool admin;
-            for (uint256 ii; ii < administrators.length; ++ii) {
-                if (administrators[ii] == _user) {
+            for (uint256 i; i < administrators.length; ++i) {
+                if (administrators[i] == _user) {
                     admin = true;
                 }
             }
